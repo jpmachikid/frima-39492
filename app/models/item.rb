7853f714749695c52a6a65e0685a6ead987cddf8
jpmachikid <d.machiki@gmail.com>
2023-08-13 06:52:13 +0900

    validationの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :user
+    validates :item_name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :price
+  end
+
+  belongs_to :user
 end
