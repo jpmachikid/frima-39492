56de354c30b693541e17eadab3031e3c700e96c1
jpmachikid <d.machiki@gmail.com>
2023-08-12 18:15:51 +0900

    テストコード修正とコメントの削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
 
     nickname {Faker::Name.name}
     email {Faker::Internet.email}
-    password {Faker::Internet.password(min_length: 6)}
+    password {Faker::Lorem.characters(number: 10,min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
     last_name{person.last.kanji}
     first_name{person.first.kanji}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,8 +52,8 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include ("Birthday can't be blank")
       end
-      it 'passwordが5文字以下では登録できない' do
-        @user.password = '11111'
+      it 'passwordが英数字混合でも5文字以下では登録できない' do
+        @user.password = 'a1111'
         @user.valid?
         expect(@user.errors.full_messages).to include ("Password is too short (minimum is 6 characters)")
       end
@@ -62,7 +62,21 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include ("Password is too long (maximum is 128 characters)")
       end
-
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Password is invalid")
+      end
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Password is invalid")
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'a1111１'
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Password is invalid")
+      end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = '12345678'
         @user.password_confirmation = '12345677'
@@ -81,6 +95,58 @@ RSpec.describe User, type: :model do
         @user.email = 'hoge'
         @user.valid?
         expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+
+      it '姓（全角）に半角文字が含まれていると登録できない' do
+        @user.last_name = 'hoge'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+      end
+
+      it '名（全角）に半角文字が含まれていると登録できない' do
+        @user.first_name = 'hoge'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
+      end
+      it '姓（カナ）にカタカナ以外の文字（平仮名）が含まれていると登録できない' do
+        @user.last_name_kana = 'ああ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+      it '姓（カナ）にカタカナ以外の文字（漢字）が含まれていると登録できない' do
+        @user.last_name_kana = '阿'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+      it '姓（カナ）にカタカナ以外の文字（英数字）が含まれていると登録できない' do
+        @user.last_name_kana = 'hoge1'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+      it '姓（カナ）にカタカナ以外の文字（記号）が含まれていると登録できない' do
+        @user.last_name_kana = '!!'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+      it '名（カナ）にカタカナ以外の文字（平仮名）が含まれていると登録できない' do
+        @user.first_name_kana = 'ああ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+      it '名（カナ）にカタカナ以外の文字（漢字）が含まれていると登録できない' do
+        @user.first_name_kana = '阿'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+      it '名（カナ）にカタカナ以外の文字（英数字）が含まれていると登録できない' do
+        @user.first_name_kana = 'hoge1'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+      it '名（カナ）にカタカナ以外の文字（記号）が含まれていると登録できない' do
+        @user.first_name_kana = '!!'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
       end
     end
   end
