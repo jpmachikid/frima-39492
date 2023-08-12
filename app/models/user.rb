bd5cb7b361c54c8b97bb5612006ef919425acd53
jpmachikid <d.machiki@gmail.com>
2023-08-12 10:49:46 +0900

    format修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 with_options presence: true do
   validates :nickname
-  validates :last_name, format:{with:/\A[ぁ-んァ-ヶ一-龥]/+\z}
-  validates :first_name, format:{with:/\A[ぁ-んァ-ヶ一-龥]/+\z}
-  validates :last_name_kana, format:{with:/\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana,format:{with:/\A[ァ-ヶー－]+\z/}
+  validates :last_name, format:{with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :first_name, format:{with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :last_name_kana, format:{with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana,format:{with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday
 end
 validates :password, length:{minimum: 6}, format:{with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
