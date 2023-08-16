079b12336c22c146ff6d04a6a1c0cf4430d72504
jpmachikid <d.machiki@gmail.com>
2023-08-16 21:05:53 +0900

    priceのvalidation修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
     validates :shipping_fee_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price, format: {with: /\A[0-9]+\z/}, numericality: { greater_than: 299, less_than: 10_000_000}
+    validates :price,  numericality: {only_integer: true, greater_than: 299, less_than: 10_000_000}
     validates :image
   end
  
