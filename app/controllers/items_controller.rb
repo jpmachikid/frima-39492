67765c4753fabf182aa6d356c13a5516b7ca9bcf
jpmachikid <d.machiki@gmail.com>
2023-08-18 08:39:15 +0900

    authenticate_user!に書き換え

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_sign_in,except: [:index]
+  before_action :authenticate_user!,except: [:index]
   def index
     @items = Item.includes(:user).order(created_at: :DESC)
   end
@@ -22,9 +22,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name,:description,:category_id,:condition_id,:shipping_fee_id,:prefecture_id,:shipping_date_id,:price,:image).merge(user_id: current_user.id)
   end
-  def move_to_sign_in
-    unless user_signed_in?
-      redirect_to '/users/sign_in'
-    end
-  end
+  
 end
