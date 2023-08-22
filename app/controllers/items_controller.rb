b6ccb8f434711e19ac9fabf0f6d2649f937854ef
jpmachikid <d.machiki@gmail.com>
2023-08-22 15:45:21 +0900

    同一コードをbefore_actionにまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index,:show]
+  before_action :find_item,only: [:show,:edit,:update]
   def index
     @items = Item.includes(:user).order(created_at: :DESC)
   end
@@ -18,20 +19,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if user_signed_in? && current_user.id != @item.user.id
       redirect_to root_path
     end
   end
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
     
-      redirect_to @item
+      redirect_to item_path(@item)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -41,8 +39,8 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name,:description,:category_id,:condition_id,:shipping_fee_id,:prefecture_id,:shipping_date_id,:price,:image).merge(user_id: current_user.id)
   end
-  
-  
-   
+  def find_item
+    @item = Item.find(params[:id])
+  end
 
 end
