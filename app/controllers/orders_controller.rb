8da9ca2d0c4a649ad2ee1464cd20657a8a3e6558
jpmachikid <d.machiki@gmail.com>
2023-09-07 07:17:20 +0900

    shipmentテーブルの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,4 +11,5 @@ class OrdersController < ApplicationController
   end
 
  
+ 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_26_074344) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_221142) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,6 +63,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_26_074344) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "shipments", charset: "utf8", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "house_number", null: false
+    t.string "building"
+    t.string "telephone", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_shipments_on_order_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -86,4 +99,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_26_074344) do
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
+  add_foreign_key "shipments", "orders"
 end
