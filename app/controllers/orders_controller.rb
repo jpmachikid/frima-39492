dbeffe30504d80d1ad1957ee5728ff5aae51766d
jpmachikid <d.machiki@gmail.com>
2023-09-07 06:54:34 +0900

    orders 遷移への条件付け

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,14 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    if user_signed_in? && current_user.id == @item.user.id
+      redirect_to root_path
+    end
+
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
+
+ 
 end
