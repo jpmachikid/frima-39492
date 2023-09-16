df2b73098a5a2ad37e887a447da788a77b68ba6f
jpmachikid <d.machiki@gmail.com>
2023-09-16 16:59:00 +0900

    log inしていないユーザーをログインページへ送る

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action authenticate_user!
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_shipment = OrderShipment.new
